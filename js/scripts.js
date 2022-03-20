56fcb1bf0b5181dcc99c1506b554ef2a32b7a4a9
DenisMuga <denismugah5@gmail.com>
2022-03-20 13:04:06 +0300

    chore: declaring pizza values like size name and crust to store the options selected

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,22 @@ function GetPizza(name, crust, topping, size, total){
 
 $(document).ready(function(){
     $("button.continue").click(function(event){
-        
-    })
+        let pname = $(".name option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let psize = $("#size option:selected").val();
+        let ptopping = [];
+        $.each($("input[name = 'toppings']:checked"), function(){
+            ptopping.push($(this).val());
+        });
+        console.log(ptopping.join(", "));
+
+        switch(psize){
+            case "0":
+            price = 0;
+            break;
+        }
+
+
+    });
 
 });
