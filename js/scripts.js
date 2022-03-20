78c2782940425617073baae9ed678089207c0097
DenisMuga <denismugah5@gmail.com>
2022-03-20 12:45:52 +0300

    chore: adding a js construct to hold various calculation variables like crust and toppings

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var price, cust_price, topping_price;
+let total = 0;
+function GetPizza(name, crust, topping, size, total){
+    this.name = name;
+    this.crust = crust;
+    this.topping = topping;
+    this.size = size;
+    this.total = total;
+}
+
+$(document).ready(function(){
+    $("button.continue").click(function(event){
+        
+    })
+
+});
